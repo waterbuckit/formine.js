c9808cd69c8e3961d81026ccc42cfe3a23648f78
Adam Royal <adamroyal@02FXECXMD6M.local>
2022-01-05 23:48:41 +0000

    Applied new abstraction

--- src/lib/reducers.js
+++ src/lib/reducers.js
@@ -1,22 +1,23 @@
 import Formine from "../formine";
 
+
 export const conditionReducer = (submission, operation = Formine.OPERATORS.AND) => (toShow, condition) => {
 
     if(!(condition.operation.prototype instanceof Formine.OPERATORS.Operator)){
         throw new Error(`Invalid Operator Type: ${typeof condition.operation}`);
     }
 
+    const applyConditions = (ret) => {
+        return operation.operate(ret == (condition.display ?? true), toShow);
+    }
+
     let ret = toShow;
     switch(condition.type){
         case "set":
-            ret = condition.conditions.reduce(conditionReducer(submission, condition.operation), true);
-            break;
+            return applyConditions(condition.conditions.reduce(conditionReducer(submission, condition.operation), true));
         case "simple":
-            ret = condition.operation.operate(submission[condition.path], condition.value);
-            break;
+            return applyConditions(condition.operation.operate(submission[condition.path], condition.value));
         case "advanced":
         default:
     }
-    ret = operation.operate(ret == (condition.display ?? true), toShow);
-    return ret;
 };
