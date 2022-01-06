4e514f0287a040c5e3d2253dfcd18982a4f8116a
Adam Royal <adamroyal@02FXECXMD6M.local>
2022-01-06 23:56:06 +0000

    Added support for first-class functions and strings as conditions

--- src/lib/reducers.js
+++ src/lib/reducers.js
@@ -1,23 +1,26 @@
 import Formine from "../formine";
 
 
-export const conditionReducer = (submission, operation = Formine.OPERATORS.AND) => (toShow, condition) => {
-
-    if(!(condition.operation.prototype instanceof Formine.OPERATORS.Operator)){
-        throw new Error(`Invalid Operator Type: ${typeof condition.operation}`);
-    }
+export const conditionReducer = (submission, operation = Formine.OPERATORS.AND) => 
+    (toShow, condition) => {
 
     const applyConditions = (ret) => {
         return operation.operate(ret == (condition.display ?? true), toShow);
     }
 
-    let ret = toShow;
-    switch(condition.type){
-        case "set":
-            return applyConditions(condition.conditions.reduce(conditionReducer(submission, condition.operation), true));
-        case "simple":
-            return applyConditions(condition.operation.operate(submission[condition.path], condition.value));
-        case "advanced":
+    switch(typeof condition){
+        case "object":
+            if(!(condition?.operation?.prototype instanceof Formine.OPERATORS.Operator)){
+                throw new Error(`Invalid Operator Type: ${typeof condition?.operation}`);
+            }
+            return condition.conditions ? 
+                applyConditions(condition.conditions.reduce(conditionReducer(submission, condition.operation), true)) : 
+                applyConditions(condition.operation.operate(submission[condition.path], condition.value));
+        case "string":
+            return applyConditions(Function('"use strict";return (' + condition + ')')()(submission));
+        case "function":
+            return applyConditions(condition(submission) ?? true);
         default:
+            throw new Error(`Invalid Condition Type: ${typeof condition}`); 
     }
 };
