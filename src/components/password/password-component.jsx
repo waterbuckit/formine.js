09c6b2ee7cce0cafde0ff6b922de682d3a2e1707
Adam Royal <adamroyal@Pauls-MacBook-Pro.local>
2022-02-09 18:11:29 +0000

    Corrected password type'

--- src/components/password/password-component.jsx
+++ src/components/password/password-component.jsx
@@ -4,5 +4,5 @@ import { useTextComponent } from "../../lib/hooks";
 export default function PasswordComponent(props) {
 	const [value, setValue, componentProps] = useTextComponent(props);
 
-	return <input {...props?.attributes} {...componentProps}  type="text" />;
+	return <input {...props?.attributes} {...componentProps}  type="password" />;
 }
