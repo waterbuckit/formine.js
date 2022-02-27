85a2e888262cbed671109d1bcaccb9d705a42297
Adam Royal <adamroyal@Adams-Machine.local>
2022-02-27 20:56:36 +0000

    Removed constructor

--- src/formine.jsx
+++ src/formine.jsx
@@ -3,14 +3,6 @@ import { h, render as preactRender } from "preact";
 import { mapToPaths } from "./lib/helpers";
 
 class Formine extends FormComponent {
-	constructor(element, schema, values = {}, options, hooks) {
-		super();
-		this.element = element;
-		this.values = mapToPaths(values); 
-		this.schema = schema;
-		this.options = options;
-		this.hooks = hooks;
-	}
 
 	static async render(element, { schema = {}, hooks = {}, values = {}, options = {} }) {
 		return new Promise((resolve, reject) => {
